c6b5c419f1f5044d8cebe6019fa748046bdad7ed
ninjatiger06 <108559006+ninjatiger06@users.noreply.github.com>
2024-05-20 21:20:46 -0700

    Added model components

--- TODO.md
+++ TODO.md
@@ -1,3 +1,1 @@
-* [ ] Fix weeks (for irregular seasons)
-  * [ ] Instead of doing yr-2020 * 17, just zero at the most recent week
-  * [ ] It's more broken
+* [ ] Fix seasons data
--- modelClass.py
+++ modelClass.py
@@ -41,7 +41,75 @@ class Model:
 
 		self.model.add(layers.Flatten())
 
+		self.model.add(layers.Dense(340, activation=activations.relu))
+
+		self.optimizer = optimizers.Adam(learning_rate=self.lr_scheduler)
+		# self.optimizer = optimizers.Adam(learning_rate=0.00001)
+		self.loss = losses.CategoricalCrossentropy()
+		self.model.compile(
+			loss = self.loss,
+			optimizer = self.optimizer,
+			metrics = ['accuracy'],
+		)
+
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-model = Model(1, 17, 3)
-# this hsould be 17 by number of inputs (stride length?)
+model = Model(17, 20, 3)
+model.model.summary()
+
+save_path = "model/"
+plotHistoryPath = "modelHistory.json"
+
+train, validation = utils.image_dataset_from_directory(
+	'keptPokemon',
+	label_mode = 'categorical',
+	batch_size = 256,
+	image_size = (239, 239),
+	seed = 69,
+	validation_split = 0.15,
+	subset = 'both'
+)
+
+train = train.cache().prefetch(buffer_size = data.AUTOTUNE)
+validation = validation.cache().prefetch(buffer_size = data.AUTOTUNE)
+
+# load previous weights if they exist
+model.model.load_weights(save_path)
+
+cpCallback = tf.keras.callbacks.ModelCheckpoint(filepath = save_path, save_weights_only = True, verbose = 1)
+
+history = model.model.fit(
+	train,
+	batch_size = 256,
+	epochs = 30,
+	verbose = 1,
+	validation_data = validation,
+	validation_batch_size = 32
+)
+
+print(f"Saving model to {save_path}")
+model.model.save(save_path)
+
+import json
+print(f"Saving training history to {plotHistoryPath}")
+
+old_history = {
+	"accuracy": [],
+	"loss": [],
+	"val_accuracy": [],
+	"val_loss": [],
+}
+try:
+	with open(plotHistoryPath, "r") as f:
+		old_history = json.load(f)
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+	pass
+
+if old_history is not None:
+	old_history["accuracy"] += history.history["accuracy"]
+	old_history["loss"] += history.history["loss"]
+	old_history["val_accuracy"] += history.history["val_accuracy"]
+	old_history["val_loss"] += history.history["val_loss"]
+
+with open(plotHistoryPath, "w") as f:
+	json.dump(old_history, f, indent=4)
